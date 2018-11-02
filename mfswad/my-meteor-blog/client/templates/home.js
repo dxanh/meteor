d23ea79e12131a1a70001bd2ffaa7b054a802fd0
dxanh <xuananhnp@gmail.com>
2018-11-02 15:10:01 +0700

    finish chap 2

--- mfswad/my-meteor-blog/client/templates/home.js
+++ mfswad/my-meteor-blog/client/templates/home.js
@@ -5,32 +5,32 @@ Template.home.created = function () {
 Template.home.rendered = function () {
     console.log('Rendered the home template');
 
-    this.$('p').html('We just replaced that text!');
+    // this.$('p').html('We just replaced that text!');
 };
 
 
-Template.home.destroyed = function () {
-    console.log('Destroyed the home template');
-};
+// Template.home.destroyed = function () {
+//     console.log('Destroyed the home template');
+// };
 
 
 // Template helpers
 Template.home.helpers({
-    exampleHelper: function () {
-        return 'This text came from a helper with some <strong>HTML</strong>.';
-        // if you use the version below, you can change the triple stache with two {{exampleHelper}} in your home.html
-        // return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
-    },
+    // exampleHelper: function () {
+    //     return 'This text came from a helper with some <strong>HTML</strong>.';
+    //     // if you use the version below, you can change the triple stache with two {{exampleHelper}} in your home.html
+    //     // return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+    // },
 
-    // Setting the data context for a template
-    dataContextHelper: function () {
-        return {
-            someText: 'This data was set using a helper of the parent template.',
-            someNested: {
-                text: 'That comes from "someNested.text"'
-            }
-        };
-    },
+    // // Setting the data context for a template
+    // dataContextHelper: function () {
+    //     return {
+    //         someText: 'This data was set using a helper of the parent template.',
+    //         someNested: {
+    //             text: 'That comes from "someNested.text"'
+    //         }
+    //     };
+    // },
 
     // Listing posts
     postsList: function () {
